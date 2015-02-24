ae10369f11a39e70eacb488a0cbf171ed638ff61
rutgervw <rutgervanwilligen@gmail.com>
2015-02-24 16:06:49 +0100

    synchronous import, always pits first. closes #1

--- index.js
+++ index.js
@@ -10,51 +10,56 @@ var fs = require('fs'),
     ];
 
 fs.readdir(config.data, function(err, directories){
-  directories.forEach(function (dir) {
-    if (dir != '.') {
-      fs.stat(config.data + "/" + dir, function (err, stat) {
-        if (stat.isDirectory()) {
-          var layer = dir;
+  async.eachSeries(files, function(file, callback) {
+    async.eachSeries(directories, function(dir, callback) {
+      if (dir != '.') {
+        fs.stat(config.data + "/" + dir, function (err, stat) {
+          if (stat.isDirectory()) {
+            var layer = dir;
 
-          async.eachSeries(files, function(file, callback) {
-            var filePath = config.data + "/" + dir + '/' + dir + '.' + file + '.ndjson',
-                base = path.basename(filePath);
+              var filePath = config.data + "/" + dir + '/' + dir + '.' + file + '.ndjson',
+                  base = path.basename(filePath);
 
-            fs.exists(filePath, function (exists) {
-              if (exists) {
-                var url = 'layers/' + layer + "/" + base.replace(layer + ".", "");
-                    formData = {file: fs.createReadStream(filePath)};
+              fs.exists(filePath, function (exists) {
+                if (exists) {
+                  var url = 'layers/' + layer + "/" + base.replace(layer + ".", "");
+                      formData = {file: fs.createReadStream(filePath)};
 
-                request.post(config.io + url, {formData: formData}, function optionalCallback(err, res, body) {
-                  if (err) {
-                    console.error('Upload failed: '.red + base);
-                    console.error("\t" + err.code);
-                    return;
-                  }
-
-                  if (body === "OK") {
-                    console.log("Upload successful: ".green + base);
-                  } else {
-                    var message = JSON.parse(body);
-                    console.log("Upload failed: ".red + base);
-                    console.log("\t" + message.error);
-                    if (message.details) {
-                      console.log(JSON.stringify(message.details, undefined, 2).split("\n").map(function(line) {
-                        return "\t" + line;
-                      }).join("\n"));
+                  request.post(config.io + url, {formData: formData}, function optionalCallback(err, res, body) {
+                    if (err) {
+                      console.error('Upload failed: '.red + base);
+                      console.error("\t" + err.code);
+                      callback();
                     }
-                  }
 
+                    if (body === "OK") {
+                      console.log("Upload successful: ".green + base);
+                    } else {
+                      var message = JSON.parse(body);
+                      console.log("Upload failed: ".red + base);
+                      console.log("\t" + message.error);
+                      if (message.details) {
+                        console.log(JSON.stringify(message.details, undefined, 2).split("\n").map(function(line) {
+                          return "\t" + line;
+                        }).join("\n"));
+                      }
+                    }
+                    callback();
+                  });
+                } else {
+                  console.log("File not found: ".yellow + base);
                   callback();
-                });
-              } else {
-                console.log("File not found: ".yellow + base);
-                callback();
-              }
-            });
-          });
-        }
-      });
-    }
+                }
+              });
+          } else {
+            callback();
+          }
+        });
+      } else {
+        callback();
+      }
+    }, function(err) {
+      callback();
+    });
   });
 });
