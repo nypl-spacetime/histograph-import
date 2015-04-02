9eb940e89dfe547a9781dc68e85e8353d2ccce38
Bert Spaan <bert.spaan@gmail.com>
2015-04-02 16:40:35 +0200

    --clear parameter to delete sources, code style tests

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Histograph IO must be running, on host & port specified in `config.json`
 
 You can also run `node index.js <source1> <source2> ...` to only import a selection of data sources into Histograph.
 
+### Remove source(s) from Histograph
+
+histograph-import can also remove sources from Histograph.
+
+To clear __all__ sources:
+
+    node index.js --clear
+
+To clear a selection of sources, run
+
+    node index.js --clear <source1> <source2> ...
--- index.js
+++ index.js
@@ -1,54 +1,77 @@
-var fs = require('fs'),
-    config = require(process.env.HISTOGRAPH_CONFIG),
-    path = require('path'),
-    request = require('request'),
-    async = require('async'),
-    colors = require('colors'),
-    files = [
-      'pits',
-      'relations'
-    ],
-    args = process.argv.slice(2);
+var fs = require('fs');
+var config = require(process.env.HISTOGRAPH_CONFIG);
+var path = require('path');
+var request = require('request');
+var async = require('async');
+var files = [
+  'pits',
+  'relations'
+];
+var clear = process.argv[2] === '--clear';
+var args = process.argv.slice(clear ? 3 : 2);
 
-fs.readdir(config.data.dir, function(err, directories){
+require('colors');
+
+fs.readdir(config.data.dir, function(err, directories) {
   async.eachSeries(files, function(file, callback) {
     async.eachSeries(directories, function(dir, callback) {
       if (dir != '.') {
-        fs.stat(config.data.dir + "/" + dir, function (err, stat) {
+        fs.stat(config.data.dir + '/' + dir, function(err, stat) {
           if (stat.isDirectory()) {
             var source = dir;
-            if (args.length == 0 || args.indexOf(source) > -1) {
-              var filePath = config.data.dir + "/" + dir + '/' + dir + '.' + file + '.ndjson',
-                  base = path.basename(filePath);
-
-              fs.exists(filePath, function (exists) {
-                if (exists) {
-                  var url = 'sources/' + source + "/" + base.replace(source + ".", "");
-                      formData = {file: fs.createReadStream(filePath)};
-
-                  request.post("http://" + config.io.host + ":" + config.io.port + "/" + url, {formData: formData}, function optionalCallback(err, res, body) {
+            if (args.length === 0 || args.indexOf(source) > -1) {
+              var filePath = config.data.dir + '/' + dir + '/' + dir + '.' + file + '.ndjson';
+              var base = path.basename(filePath);
+              var response = function(err, res, body) {
                     if (err) {
                       console.error('Upload failed: '.red + base);
-                      console.error("\t" + err.code);
+                      console.error('\t' + err.code);
                       callback();
                     }
 
-                    if (body === "OK") {
-                      console.log("Upload successful: ".green + base);
+                    if (body === 'OK') {
+                      console.log('Upload successful: '.green + base);
                     } else {
-                      var message = JSON.parse(body);
-                      console.log("Upload failed: ".red + base);
-                      console.log("\t" + message.error);
+                      var message;
+                      try {
+                        message = JSON.parse(body);
+                      } catch (parseError) {
+                        message = {error: body};
+                      }
+
+                      console.log('Upload failed: '.red + base);
+                      console.log('\t' + message.error);
                       if (message.details) {
-                        console.log(JSON.stringify(message.details, undefined, 2).split("\n").map(function(line) {
-                          return "\t" + line;
-                        }).join("\n"));
+                        console.log(JSON.stringify(message.details, null, 2).split('\n').map(function(line) {
+                          return '\t' + line;
+                        }).join('\n'));
                       }
                     }
+
                     callback();
-                  });
+                  };
+
+              fs.exists(filePath, function(exists) {
+                if (exists) {
+                  var url = 'sources/' + source + '/' + base.replace(source + '.', '');
+
+                  if (clear) {
+                    // Send empty file
+
+                    request('http://' + config.io.host + ':' + config.io.port + '/' + url, {
+                      method: 'POST',
+                      headers: {'content-type': 'text/plain'},
+                      body: ''
+                    }, response);
+                  } else {
+                    var formData = {file: fs.createReadStream(filePath)};
+
+                    request.post('http://' + config.io.host + ':' + config.io.port + '/' + url, {
+                      formData: formData
+                    }, response);
+                  }
                 } else {
-                  console.log("File not found: ".yellow + base);
+                  console.log('File not found: '.yellow + base);
                   callback();
                 }
               });
@@ -62,7 +85,9 @@ fs.readdir(config.data.dir, function(err, directories){
       } else {
         callback();
       }
-    }, function(err) {
+    },
+
+    function() {
       callback();
     });
   });
